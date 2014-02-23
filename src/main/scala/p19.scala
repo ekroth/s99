517358dafa8e54f28324c750d5c4b3b0c946dba2
Andrée Ekroth <ekroth@users.noreply.github.com>
2014-02-23 01:52:23 +0100

    Update p19.scala

--- src/main/scala/p19.scala
+++ src/main/scala/p19.scala
@@ -8,7 +8,4 @@ object p19 {
     val (head, tail) = xs splitAt pos
     tail ++ head
   }
-
-  rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
-  rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
 }
